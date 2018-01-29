83dc45cc0649cc330b277d4bab4bcc598afb4aed
Derek Hansen <derek.hansen@chiefind.com>
2018-01-29 14:45:41 -0600

    T04a.02 changes

--- Lesson04a-Starting-New-Activities/T04a.02-Exercise-StartNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.02-Exercise-StartNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -62,12 +62,11 @@ public class MainActivity extends AppCompatActivity {
                 Context context = MainActivity.this;
 
                 // TODO (1) Store ChildActivity.class in a Class object called destinationActivity
-
+                Class childActivity = ChildActivity.class;
                 // TODO (2) Create an Intent to start ChildActivity
-
+                Intent intent = new Intent(context, childActivty);
                 // TODO (3) Replace the Toast with code to start ChildActivity
-                String message = "Button clicked!\nTODO: Start a new Activity and pass some data.";
-                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+                startActivity(intent);
 
             }
         });
